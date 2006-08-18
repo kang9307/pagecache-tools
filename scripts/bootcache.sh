52ebf3869f40ef27420cb01af03ab123fb65b48c
fengguang.wu <fengguang.wu@bcc9bc22-f21a-0410-94da-1172e61f2d17>
2006-08-18 08:29:09 +0000

    we can now log the startup time

--- scripts/bootcache.sh
+++ scripts/bootcache.sh
@@ -3,14 +3,63 @@
 # Provides:          bootcache
 # Required-Start:    mountkernfs
 # Required-Stop:
-# Default-Start:     S
+# Default-Start:     S 2 3 4 5
 # Default-Stop:      0 6
 # Short-Description: preload boot files on startup;
-#                    do scheduled defrag on reboot/shutdown.
+#                    do scheduled defrag on reboot/shutdown;
+#                    log the sysv-init/desktop boot time
 ### END INIT INFO
 
 CACHE_ROOT=/var/cache/bootcache
 .  /etc/default/bootcache
+
+PATH=/sbin:/bin
+grep -q bootcache /proc/cmdline && BOOTCACHE=bootcache
+
+function wait_for_process_start()
+{
+    until pidof $1 > /dev/null; do sleep 1; done
+}
+
+function wait_for_process_stop()
+{
+    while pidof $1 > /dev/null; do sleep 1; done
+}
+
+function wait_for_process()
+{
+	SIGNAL_PROGRAM=$1
+	while shift
+	do
+		if   [ "$1" = 'start' ]; then wait_for_process_start $SIGNAL_PROGRAM
+		elif [ "$1" = 'stop'  ]; then wait_for_process_stop  $SIGNAL_PROGRAM
+		else break; fi
+	done
+}
+
+function log_line()
+{
+	echo $@ >> $CACHE_ROOT/uptime
+}
+
+function log_boot_time()
+{
+	if [ -z "$BOOTTIME_LOG_WAITCMD" ]; then
+		return
+	fi
+
+	# sysv-init boot time
+	log_line "SYSV $(</proc/uptime) $BOOTCACHE \#  `date` `uname -a`"
+
+	(
+		# do the wait
+		eval $BOOTTIME_LOG_WAITCMD
+
+		# GUI desktop ready time
+		log_line "GUI  $(</proc/uptime) $BOOTCACHE"
+		log_line
+	)&
+}
 
 function check_do_defrag()
 {
@@ -28,6 +77,8 @@ case "$1" in
 	start)
 		if [ "$RUNLEVEL" = "S" -a "$PREVLEVEL" = "N" ]; then
 			bootcache preload boot
+		else
+			log_boot_time
 		fi
 		;;
 	stop)
@@ -35,4 +86,10 @@ case "$1" in
 			check_do_defrag
 		fi
 		;;
+	*)
+		echo "Usage: $0 start|stop" >&2
+		exit 3
+		;;
 esac
+
+:
